8f85a65aa885512faee0418c64f027b8ac15eacb
Matthew Flanagan <mattflanagan40@gmail.com>
2022-07-18 19:52:09 -0400

    refracted code

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var searchButton = document.querySelector(".search-button");
+    var vesselContainer = $('.vessel');
+    var nameElement = $('#name');
+    var locationElement = $('#location');
+    var statusElement = $('#status');
+    var destinationElement = $('#destination');
+
+
+})
